1b0b55d93927fc7489f0f19008a65074b8f0ce89
Pavel Bucek <pavel.bucek@oracle.com>
2014-06-16 22:55:49 +0200

    getting rid of static List<Session>

--- tyrus-test/src/main/java/pl/cmil/tyrus/Endpoint2.java
+++ tyrus-test/src/main/java/pl/cmil/tyrus/Endpoint2.java
@@ -2,9 +2,6 @@ package pl.cmil.tyrus;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,47 +14,38 @@ import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint(value = "/endpoint2")
 public class Endpoint2 {
-	private static final Logger logger = Logger.getLogger("Endpoint2");
+  private static final Logger logger = Logger.getLogger("Endpoint2");
 
-	/** All open WebSocket sessions */
-	static List<Session> peers = Collections
-			.synchronizedList(new ArrayList<Session>());
+  @OnOpen
+  public void openConnection(Session session)
+      throws IllegalArgumentException, IOException {
+    session.getBasicRemote().sendPong(ByteBuffer.wrap(new byte[10]));
 
-	@OnOpen
-	public void openConnection(Session session)
-			throws IllegalArgumentException, IOException {
-		peers.add(session);
+    logger.info("New connection opened " + session.getId());
+  }
 
-		session.getBasicRemote().sendPong(ByteBuffer.wrap(new byte[10]));
+  @OnClose
+  public void closedConnection(Session session) {
+    logger.log(Level.INFO, "closing " + session.getId());
+  }
 
-		logger.info("New connection opened " + session.getId());
-	}
+  @OnMessage
+  public void message(final Session session, String value) {
+    send(session, session.getId() + ";" + value);
 
-	@OnClose
-	public void closedConnection(Session session) {
-		peers.remove(session);
-		logger.log(Level.INFO, "closing " + session.getId());
-	}
+    logger.log(Level.INFO, "Sent: {0}", session.getId() + ";" + value);
+  }
 
-	@OnMessage
-	public void message(final Session session, String value) {
-		send(session.getId() + ";" + value);
+  public static void send(Session session, String msg) {
+    for (Session s : session.getOpenSessions()) {
+      try {
+        logger.info("Sending update to " + session.getId());
 
-		logger.log(Level.INFO, "Sent: {0}", session.getId() + ";" + value);
-	}
+        s.getBasicRemote().sendObject(msg);
 
-	public static void send(String msg) {
-		try {
-			for (Session session : peers) {
-				if (session.isOpen()) {
-					logger.info("Sending update to " + session.getId());
-
-					session.getBasicRemote().sendObject(msg);
-				}
-			}
-		} catch (IOException | EncodeException e) {
-			logger.log(Level.SEVERE, e.toString());
-		}
-	}
-
+      } catch (IOException | EncodeException e) {
+        logger.log(Level.SEVERE, e.toString());
+      }
+    }
+  }
 }
